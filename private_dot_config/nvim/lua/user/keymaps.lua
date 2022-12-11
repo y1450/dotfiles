3f9f2e8a894e932d600be61981af766af3705e16
y1450 <sunny.rahul@tutanota.com>
2022-12-11 21:59:09 +0100

    (nvim-keymaps): add most common used function to keymaps
    Related Issues : #1,#21,#22,#23

--- private_dot_config/nvim/lua/user/keymaps.lua
+++ private_dot_config/nvim/lua/user/keymaps.lua
@@ -20,7 +20,22 @@ keymap("i", "jk", "<Esc>", options)
 
 -- Git
 keymap("n", "<leader><leader>", ":Telescope git_files<cr>",opts)
+keymap("n", "<leader>gb", ":Telescope git_branches<cr>",opts)
+keymap("n", "<leader>gs", ":Git<cr>",opts)
+
 -- Ctrl d to delete worktree
 keymap("n", "<leader>gww", ":Telescope git_worktree git_worktrees<cr>",opts)
 keymap("n", "<leader>gwc", ":Telescope git_worktree create_git_worktree<cr>",opts)
 
+-- Nvim Tree
+keymap("n", "<leader>e", ":NvimTreeToggle<cr>",opts)
+keymap("n", "<leader>b", ":Telescope buffers<cr>",opts)
+-- Telecope 
+--TODO Buffer fuzzy
+--does not work , I have tried setup with fzf 
+--keymap("n", "<leader>bf", ":Telescope current_buffer_fuzzy_find<cr>",opts)
+keymap("n", "<leader>bt", ":Telescope current_buffer_tags<cr>",opts)
+keymap("n", "<leader>ff", ":Telescope find_files<cr>",opts)
+keymap("n", "<leader>ft", ":Telescope tags<cr>",opts)
+keymap("n", "<leader>rg", ":Telescope live_grep<cr>",opts)
+keymap("n", "<leader>rr", ":Telescope grep_string<cr>",opts)
