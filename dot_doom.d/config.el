006aa65e29b4f44b4b3315ed61090c1bd01ad732
y1450 <sunny.rahul@tutanota.com>
2022-12-24 14:56:46 +0100

    (org-roam) add deft for searching notes
    https://www.orgroam.com/manual.html#Full_002dtext-search-with-Deft

--- dot_doom.d/config.el
+++ dot_doom.d/config.el
@@ -98,4 +98,13 @@
 (setq org-download-screenshot-method "shotgun -g $(hacksaw) %s")
 ;;(setq org-download-screenshot-method "flameshot gui --raw > %s")
 
+(use-package deft
+  :after org
+  :bind
+  ("C-c n d" . deft)
+  :custom
+  (deft-recursive t)
+  (deft-use-filter-string-for-filename t)
+  (deft-default-extension "org")
+  (deft-directory org-roam-directory))
 
