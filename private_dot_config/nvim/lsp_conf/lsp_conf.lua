25b8d46153320ff31d2c03885f2125d01f491a44
y1450 <sunny.rahul@tutanota.com>
2023-01-01 11:50:19 +0100

    nvim (lsp) : disable lsp server startup.
    I run alot projects and running multiple lsp server eats alot of ram.
    if I need lsp, I would start it manually

--- private_dot_config/nvim/lsp_conf/lsp_conf.lua
+++ private_dot_config/nvim/lsp_conf/lsp_conf.lua
@@ -37,14 +37,17 @@ local lsp_flags = {
   debounce_text_changes = 150,
 }
 require('lspconfig')['pyright'].setup{
+    autostart = false,
     on_attach = on_attach,
     flags = lsp_flags,
 }
 require('lspconfig')['tsserver'].setup{
+    autostart = false,
     on_attach = on_attach,
     flags = lsp_flags,
 }
 require('lspconfig')['rust_analyzer'].setup{
+    autostart = false,
     on_attach = on_attach,
     flags = lsp_flags,
     -- Server-specific settings...
