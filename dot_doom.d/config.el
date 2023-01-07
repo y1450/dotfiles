227b7db75555da295ec87d07e0f930c11e4f4c99
y1450 <sunny.rahul@tutanota.com>
2023-01-07 17:59:15 +0100

    doomemacs(org-roam) : add chrisbarrett/nursery 'org-roam-review' for note processing.
    - inspired by  [maggie appleton tana workflow](https://youtu.be/rdQ4dEa5fmU?t=953)
    - relevant by [org roam forum post](https://org-roam.discourse.group/t/how-to-note-processing-pipeline/2927)

--- dot_doom.d/config.el
+++ dot_doom.d/config.el
@@ -139,3 +139,30 @@
           org-roam-ui-update-on-save t
           org-roam-ui-open-on-start t))
 
+
+(use-package org-drill :ensure t)
+(add-to-list 'load-path "/home/rahul/git/nursery/lisp")
+(use-package org-roam-review
+  :commands (org-roam-review
+             org-roam-review-list-by-maturity
+             org-roam-review-list-recently-added)
+
+  ;; ;; Optional - tag all newly-created notes as seedlings.
+  ;; :hook (org-roam-capture-new-node . org-roam-review-set-seedling)
+
+  ;; ;; Optional - keybindings for applying Evergreen note properties.
+  ;; :general
+  ;; (:keymaps 'org-mode-map
+  ;; "C-c r r" '(org-roam-review-accept :wk "accept")
+  ;; "C-c r u" '(org-roam-review-bury :wk "bury")
+  ;; "C-c r x" '(org-roam-review-set-excluded :wk "set excluded")
+  ;; "C-c r b" '(org-roam-review-set-budding :wk "set budding")
+  ;; "C-c r s" '(org-roam-review-set-seedling :wk "set seedling")
+  ;; "C-c r e" '(org-roam-review-set-evergreen :wk "set evergreen"))
+
+  ;; ;; Optional - bindings for evil-mode compatability.
+  ;; :general
+  ;; (:states '(normal) :keymaps 'org-roam-review-mode-map
+  ;; "TAB" 'magit-section-cycle
+  ;; "g r" 'org-roam-review-refresh)
+  )
--- dot_doom.d/packages.el
+++ dot_doom.d/packages.el
@@ -60,3 +60,5 @@
 
 (unpin! org-roam)
 (package! org-roam-ui)
+(package! org-drill)
+(package! ts)
